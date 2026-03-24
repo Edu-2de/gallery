26f67d24139d8547799abb55f51c45c7a4bba3e4
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-24 11:57:30 -0300

    feat: replace ImageFilePreview with new ImagePreview component; update imports in PageComponents

--- gallery-plus/src/pages/page-components.tsx
+++ gallery-plus/src/pages/page-components.tsx
@@ -10,7 +10,7 @@ import SearchIcon from "../assets/icons/search.svg?react";
 import InputCheckbox from "../components/input-checkbox";
 import InputSingleFile from "../components/input-single-file";
 import { useForm } from "react-hook-form";
-import ImagePreview from "../components/image-file-preview";
+import ImagePreview from "../components/image-preview";
 import { Dialog, DialogClose, DialogTrigger } from "@radix-ui/react-dialog";
 import {
     DialogBody,
