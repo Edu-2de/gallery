b6babcad16397c8a7fbd0195ea776023596cbd29
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-24 14:32:33 -0300

    feat: update PageHome to include multiple PhotoWidget instances; add photos-list component

--- gallery-plus/src/pages/page-home.tsx
+++ gallery-plus/src/pages/page-home.tsx
@@ -1,10 +1,11 @@
 import Container from "../components/container";
 import PhotoWidget from "../contexts/photos/components/photo-widget";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PageHome() {
     return (
-        <>
-            <Container>
+        <Container>
+            <div className="grid grid-cols-5 gap-9">
                 <PhotoWidget
                     photo={{
                         id: "123",
@@ -17,7 +18,32 @@ export default function PageHome() {
                         ],
                     }}
                 />
-            </Container>
-        </>
+                <PhotoWidget
+                    photo={{
+                        id: "123",
+                        title: "ola mundo!",
+                        imageId: "portrait-tower.png",
+                        albums: [
+                            { id: "321", title: "seila" },
+                            { id: "126", title: "outro" },
+                            { id: "423", title: "outro" },
+                        ],
+                    }}
+                />
+                <PhotoWidget
+                    photo={{
+                        id: "123",
+                        title: "ola mundo!",
+                        imageId: "portrait-tower.png",
+                        albums: [
+                            { id: "321", title: "seila" },
+                            { id: "126", title: "outro" },
+                            { id: "423", title: "outro" },
+                        ],
+                    }}
+                />
+                <PhotoWidget photo={{} as Photo} loading />
+            </div>
+        </Container>
     );
 }
