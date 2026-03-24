9318dfb0eb45740b4fece27fce5b28149cb7cdbf
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-24 15:10:48 -0300

    refactor: improve structure of PhotosList component; enhance loading and empty states

--- gallery-plus/src/contexts/photos/components/photos-list.tsx
+++ gallery-plus/src/contexts/photos/components/photos-list.tsx
@@ -1,3 +1,4 @@
+import Text from "../../../components/text";
 import type { Photo } from "../models/photo";
 import PhotoWidget from "./photo-widget";
 
@@ -9,21 +10,31 @@ interface PhotosListProps {
 export default function PhotosList({ photos, loading }: PhotosListProps) {
     return (
         <div>
-            <div className="grid grid-cols-5 gap-9">
-                {!loading &&
-                    photos?.length > 0 &&
-                    photos.map((photo) => (
+            {!loading && photos?.length > 0 && (
+                <div className="grid grid-cols-5 gap-9">
+                    {photos.map((photo) => (
                         <PhotoWidget key={photo.id} photo={photo} />
                     ))}
-                {loading &&
-                    Array.from({ length: 10 }).map((_, index) => (
+                </div>
+            )}
+            {loading && (
+                <div className="grid grid-cols-5 gap-9">
+                    {Array.from({ length: 10 }).map((_, index) => (
                         <PhotoWidget
                             key={`photo-loading-${index}`}
                             photo={{} as Photo}
                             loading
                         />
                     ))}
-            </div>
+                </div>
+            )}
+            {!loading && photos?.length === 0 && (
+                <div className="flex justify-center items-center h-full">
+                    <Text variant="paragraph-large" className="text-accent-red">
+                        Nenhuma foto encontrada!
+                    </Text>
+                </div>
+            )}
         </div>
     );
 }
