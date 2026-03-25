414658d26936ff9a08f5e1d97bf54a323395ba0d
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-25 15:02:17 -0300

    feat: add loading state to AlbumsListSelectable component in PagePhotoDetail

--- gallery-plus/src/contexts/albums/components/albums-list-selectable.tsx
+++ gallery-plus/src/contexts/albums/components/albums-list-selectable.tsx
@@ -4,6 +4,7 @@ import type { Photo } from "../../photos/models/photo";
 import Text from "../../../components/text";
 import InputCheckbox from "../../../components/input-checkbox";
 import Divider from "../../../components/divider";
+import Skeleton from "../../../components/skeleton";
 
 interface AlbumsListSelectable extends React.ComponentProps<"div"> {
     loading?: boolean;
@@ -14,24 +15,31 @@ interface AlbumsListSelectable extends React.ComponentProps<"div"> {
 export default function AlbumsListSelectable({
     loading,
     albums,
-    photo,
-    ...props
 }: AlbumsListSelectable) {
     return (
         <ul className="flex flex-col gap-4">
-            {albums.map((album, index) => (
-                <li key={album.id}>
-                    <div className="flex items-center justify-between gap-1">
-                        <Text variant="paragraph-large" className="truncate">
-                            {album.title}
-                        </Text>
-                        <InputCheckbox />
-                    </div>
-                    {index !== albums.length - 1 && (
-                        <Divider className="mt-4" />
-                    )}
-                </li>
-            ))}
+            {!loading && albums.length > 0
+                ? albums.map((album, index) => (
+                      <li key={album.id}>
+                          <div className="flex items-center justify-between gap-1">
+                              <Text
+                                  variant="paragraph-large"
+                                  className="truncate"
+                              >
+                                  {album.title}
+                              </Text>
+                              <InputCheckbox />
+                          </div>
+                          {index !== albums.length - 1 && (
+                              <Divider className="mt-4" />
+                          )}
+                      </li>
+                  ))
+                : Array.from({ length: 5 }).map((_, index) => (
+                      <li key={`albums-list=${index}`}>
+                        <Skeleton className="h-[2.5rem]"></Skeleton>
+                      </li>
+                  ))}
         </ul>
     );
 }
