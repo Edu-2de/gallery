1bb23fbd7a24b9372951c558026e40a67a1a42a5
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-26 15:40:31 -0300

    feat: wrap application in QueryClientProvider for improved API state management

--- gallery-plus/src/App.tsx
+++ gallery-plus/src/App.tsx
@@ -1,19 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PageComponents from "./pages/page-components";
 import LayoutMain from "./pages/layout-main";
 import PageHome from "./pages/page-home";
 import PagePhotoDetail from "./pages/page-photo-detail";
 
+const queryClient = new QueryClient();
+
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route element={<LayoutMain />}>
-                    <Route index element={<PageHome />} />
-                    <Route path="/componentes" element={<PageComponents />} />
-                    <Route path="/fotos/:id" element={<PagePhotoDetail />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<LayoutMain />}>
+                        <Route index element={<PageHome />} />
+                        <Route
+                            path="/componentes"
+                            element={<PageComponents />}
+                        />
+                        <Route
+                            path="/fotos/:id"
+                            element={<PagePhotoDetail />}
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     );
 }
