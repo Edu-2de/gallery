24ad29b2f5e6a51418b80e3aa865c8b9af05520b
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-31 16:30:01 -0300

    feat: atualizar lógica de criação de álbuns para incluir IDs de álbuns existentes nas fotos

--- gallery-plus/src/contexts/albums/hooks/use-album.ts
+++ gallery-plus/src/contexts/albums/hooks/use-album.ts
@@ -1,11 +1,13 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { api } from "../../../helpers/api";
+import usePhotos from "../../photos/hooks/use-photos";
 import type { Album } from "../models/albums";
 import type { AlbumNewFormSchema } from "../schemas";
 
 export default function useAlbum() {
     const queryClient = useQueryClient();
+    const { photos } = usePhotos();
 
     async function createAlbum(payload: AlbumNewFormSchema) {
         try {
@@ -16,8 +18,13 @@ export default function useAlbum() {
             if (payload.photosIds && payload.photosIds.length > 0) {
                 await Promise.all(
                     payload.photosIds.map((photoId) => {
+                        const photoAlbumsIds =
+                            photos
+                                .find((photo) => photo.id === photoId)
+                                ?.albums?.map((album) => album.id) ?? [];
+
                         return api.put(`/photos/${photoId}/albums`, {
-                            albumsIds: [album.id],
+                            albumsIds: [...photoAlbumsIds, album.id],
                         });
                     }),
                 );
