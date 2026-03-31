ce7b71b2e6ffbef5aeebd3211db7785497857669
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-31 14:29:09 -0300

    feat: adicionar biblioteca Sonner para notificações e integrar ao fluxo de criação de fotos

--- gallery-plus/src/contexts/photos/hooks/use-photo.ts
+++ gallery-plus/src/contexts/photos/hooks/use-photo.ts
@@ -1,4 +1,5 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../schemas";
@@ -18,7 +19,6 @@ export default function usePhoto(id?: string) {
     const queryClient = useQueryClient();
 
     async function createPhoto(payload: PhotoNewFormSchema) {
-        // eslint-disable-next-line no-useless-catch
         try {
             const { data: photo } = await api.post<Photo>("/photos", {
                 title: payload.title,
@@ -41,13 +41,14 @@ export default function usePhoto(id?: string) {
                     albumsIds: payload.albumsIds,
                 });
             }
+
+            queryClient.invalidateQueries({ queryKey: ["photos"] });
+            toast.success("Foto criada com sucesso");
         } catch (error) {
+            toast.error("Erro ao criar a foto");
             throw error;
         }
-
-        queryClient.invalidateQueries({ queryKey: ["photos"] });
     }
-
     return {
         photo: data,
         nextPhotoId: data?.nextPhotoId,
